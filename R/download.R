6c4608185e922695053ad160b6bdda1397b75956
Andreas Beger <adbeger@gmail.com>
2023-06-30 14:21:23 +0300

    skip data file download if already present

--- R/download.R
+++ R/download.R
@@ -7,17 +7,92 @@
 #' @param years Do you want to download data for only specific years? The
 #'   default (NULL) will download all available data. Otherwise, specify a
 #'   numeric vector, e.g. \code{2023}, \code{2022:2023}, or \code{c(2021, 2023)}.
+#' @param skip_existing Skip files that are already in "local_dir"?
 #' @param verbose Print progress messages as files are being downloaded?
+#' @param dryrun Don't actually download the files, just print information on
+#'   what files would be downloaded.
+#'
+#' @details This function will download files from dataverse as they are, which
+#'   namely means that the zipped files will remain zipped. Two potential issues:
+#'
+#'   - You zipped the "ReleaseXXX.DV.txt" files to save space: "skip_existing"
+#'     should be able to handle that. It will remove ".zip" or ".gz" from the
+#'     filenames before comparing.
+#'   - You unzipped the yearly data files, which actually each contain 12
+#'     monthly data files. To avoid re-downloading those, use "skip_existing"
+#'     and set the "years" argument to only the current year. See the examples.
 #'
 #' @returns Data will be downloaded to \code{local_dir}.
 #'
+#' @examples
+#' \dontrun{
+#'   # Basic, fresh download
+#'   download_polecat("my/data/dir")
+#'
+#'   # Next time, to avoid re-downloading files you already have:
+#'   download_polecat("my/data/dir", skip_existing = TRUE)
+#'
+#'   # To download only fresh data fur the current year, or avoid re-downloading
+#'   # the yearly historical data files if you unzipped them:
+#'   download_polecat("my/data/dir", years = 2023, skip_existing = TRUE)
+#' }
 #' @export
-download_polecat <- function(local_dir, years = NULL, verbose = TRUE) {
+#' @md
+download_polecat <- function(local_dir, years = NULL, skip_existing = TRUE,
+                             verbose = TRUE, dryrun = FALSE) {
+  # Check arguments
+  stopifnot(
+    "'skip_existing' must be logical" = is.logical(skip_existing),
+    "'verbose' must be logical" = is.logical(verbose),
+    "'dryrun' must be logical" = is.logical(dryrun)
+  )
+  if (!dir.exists(local_dir)) {
+    msg <- sprintf("Directory '%s' does not exist, create it first with `dir.create(%s)`",
+                   local_dir, local_dir)
+    stop(msg)
+  }
+
   dataverse_files <- get_dataverse_file_list()
 
   if (!is.null(years)) {
     years <- as.integer(years)
-    dataverse_files <- dataverse_files[dataverse_files$year %in% years]
+    dataverse_files <- dataverse_files[dataverse_files$data_year %in% years, ]
+  }
+
+  # Do we need to check for an exclude already present local files?
+  skip_n <- NA_integer_  # initialize in case we do a dry run and need it for message
+  if (skip_existing) {
+    local_files <- dir(local_dir)
+
+    # remove any .zip or .gz extensions in case the files are locally zipped
+    local_files <- gsub("\\.zip$|\\.gz$", "", local_files)
+
+    already_present <- dataverse_files$label %in% local_files
+    if (any(already_present)) {
+      skip_n <- sum(already_present)
+      dataverse_files <- dataverse_files[!already_present, ]
+
+      # don't print this if dryrun because we'll print it below
+      if (verbose & !dryrun) {
+        cat(sprintf("Skipping download for %s files that are already present\n",
+                    skip_n))
+      }
+    }
+  }
+
+  # Early exit if this is a dryrun
+  if (dryrun) {
+    cat("Dryrun\n")
+    cat(sprintf("Local dir is '%s'\n", local_dir))
+    if (!is.na(skip_n)) {
+      cat(sprintf("Skipping download for %s files that are already present\n",
+                  skip_n))
+    }
+    cat(paste0(
+      sprintf("Downloading %s files:\n", nrow(dataverse_files)),
+      paste0(dataverse_files$label, collapse = "\n")
+    ))
+    return(invisible(TRUE))
   }
 
   for (i in seq_len(nrow(dataverse_files))) {
--- README.md
+++ README.md
@@ -6,9 +6,12 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of rpolecat is to make downloading and working with the
-[POLECAT event data](https://dataverse.harvard.edu/dataverse/POLECAT)
-easier.
+rpolecat makes downloading and working with the [POLECAT event
+data](https://dataverse.harvard.edu/dataverse/POLECAT) easier.
+
+The main download functionality works, but there’s more stuff in the
+works, see our roadmap in
+<https://github.com/basil-analytics/rpolecat/issues/1>.
 
 The POLECAT data are provided via two repositories on dataverse:
 
@@ -17,6 +20,15 @@ The POLECAT data are provided via two repositories on dataverse:
   containing the actual data files.
 - [POLECAT
   Documentation](https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LMFPIP)
+
+The data are described in two papers:
+
+- [PLOVER and POLECAT: A New Political Event Ontology and
+  Dataset](https://osf.io/preprints/socarxiv/rm5dw/): data paper with
+  details on the POLECAT data and PLOVER ontology.
+- [Creating Custom Event Data Without Dictionaries: A Bag-of-Tricks
+  (arXiv)](https://arxiv.org/abs/2304.01331): details on the NGEC coder
+  that produces the data.
 
 ## Installation
 
@@ -27,9 +39,59 @@ library(remotes)
 install_github("basil-analytics/rpolecat")
 ```
 
+rpolecat depends on the [R Dataverse
+Client](https://github.com/IQSS/dataverse-client-r) to interact with the
+Dataverse API. That package requires two environment variables in order
+to be able to interact with the API. More details are documented in the
+dataverse R package readme [API Access
+Keys](https://github.com/IQSS/dataverse-client-r#api-access-keys)
+section.
+
+One way to meet this requirement is to:
+
+1.  [Obtain an API access
+    token](https://guides.dataverse.org/en/latest/user/account.html#api-token)
+    from Harvard Dataverse.
+2.  Add the following lines to your
+    [`.Rprofile`](https://support.posit.co/hc/en-us/articles/360047157094-Managing-R-with-Rprofile-Renviron-Rprofile-site-Renviron-site-rsession-conf-and-repos-conf)
+    file:
+
+``` r
+# dataverse API token
+Sys.setenv(DATAVERSE_SERVER = "dataverse.harvard.edu")
+Sys.setenv(DATAVERSE_KEY = "<your API token>")
+```
+
+(You can find and open your `.Rprofile` file using
+`usethis::edit_r_profile()` if the
+[usethis](https://cran.r-project.org/package=usethis) package is
+installed.) 3. Restart R for the changes to take effect.
+
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
+One of the main functions of this package is to download the POLECAT
+data without manual point and clicking on Dataverse:
+
+``` r
+# (not run)
+
+# one-stop-shop for getting all data and keeping updated with new data:
+# 1st time
+download_polecat(local_dir = "my/data/dir", skip_exiting = TRUE)
+# next times
+download_polecat(local_dir = "my/data/dir", years = 2023, skip_existing = TRUE)
+
+# see ?download_polecat
+```
+
+This will download the files as they are, namely the current year weekly
+files will remain unzipped, and the historical data yearly ZIP archives
+will remain zipped. However, the “skip_existing” functionality *should*
+be able to handle things correctly if you zip/unzip various files, see
+`?download_polecat` for more details.
+
+The package also include information from the [PLOVER
+ontology](https://github.com/openeventdata/PLOVER/blob/master/PLOVER_MANUAL.pdf):
 
 ``` r
 library(rpolecat)
@@ -53,18 +115,6 @@ head(modes)
 #> 4    consult        phone
 #> 5    retreat     withdraw
 #> 6    retreat      release
-```
-
-To download the data:
-
-``` r
-# (not run)
-
-# to donwload all data
-download_polecat(local_dir = "foo/foo")
-
-# to download only some years
-download_polecat(local_dir = "foo/foo", years = c(2021:2023))
 ```
 
 ## Data license
